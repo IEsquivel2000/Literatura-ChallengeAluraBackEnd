80a0feeb0d5f745286276dfbb8a0fedbe0d949f3
IEsquivel2000 <iesquivel200010@gmail.com>
2024-07-13 12:03:18 -0500

    Initialize commit

---
+++ literatura-ChallengeAluraBackEnd/src/main/java/com/alura/literatura_ChallengeAluraBackEnd/LiteraturaChallengeAluraBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.alura.literatura_ChallengeAluraBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiteraturaChallengeAluraBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiteraturaChallengeAluraBackEndApplication.class, args);
+	}
+
+}
---
+++ literatura-ChallengeAluraBackEnd/src/test/java/com/alura/literatura_ChallengeAluraBackEnd/LiteraturaChallengeAluraBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alura.literatura_ChallengeAluraBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiteraturaChallengeAluraBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
